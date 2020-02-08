ce3a8c2bc3a8ffba2ff15d6d5dcfbe53ac27c1c6
Jonathan <jonathancb.12@gmail.com>
2020-02-08 18:36:12 -0300

    primera prueba listar usuarios

--- src/main/java/com/portal/web/app/controller/IndexController.java
+++ src/main/java/com/portal/web/app/controller/IndexController.java
@@ -1,13 +1,22 @@
 package com.portal.web.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.portal.web.app.model.DAO.IUsuarioDao;
 
 @Controller
 public class IndexController {
 
+	@Autowired
+	private IUsuarioDao usuarioDao;
+	
 	@RequestMapping(value = { "/home", "/", "/inicio", "/index" })
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("titulo", "Listado de Usuarios");
+		model.addAttribute("usuarios",usuarioDao.findAll());
 		return "index";
 	}
 }
